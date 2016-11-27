22a7e65de9c872f5ed2cd3182ec787b67572ed52
james0x41 <james0x41@gmail.com>
2016-11-28 01:27:55 +0800

    use get_xid to get the xid of the given index

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@
 
 # lxml parses xml and html docs very well
 from lxml import html
+from lxml import etree
 # requests instead of urllib2 because of readability and speed
 import requests
 from pprint import pprint
@@ -10,24 +11,40 @@ import time
 
 from utils.IOCSV import IOCSV
 
+# default global variables
+_index = 'INX:IOM'
+file_path = './data'
+file_name = 'data01'
+
+
 # get xid from an index (i.e INX:IOM, FTSE:FSI, etc)
 def get_xid(index):
     payload = {'s': index}
-    website = "https://markets.ft.com/data/indices/tearsheet/constituents"
-    page = requests.get(website, params=payload)
+    xidSite = "https://markets.ft.com/data/indices/tearsheet/constituents"
+    page = requests.get(xidSite, params=payload)
     tree = html.fromstring(page.content)
-    xid = tree.xpath('//div[@class=')
+    xid = tree.xpath('/html/body/div[3]/div[3]/section/div[1]/div/div/div')
+    if xid:
+        return xid[0].attrib['data-mod-xid']
+    else:
+        print 'Did not find the xid for', index
 
 
-if __name__ == '__main__':
-    file_path = './data'
-    file_name = 'data01'
+def get_user_input():
+    print 'Leave blank for defaults'
+    index = raw_input('Index to scrape (default: ' + _index + '> ')
+    if index:
+        xid = get_xid(index)
+    else:
+        xid = get_xid(_index)
+
+
+def main():
     # create an instance of IOJson
     saveData = IOCSV(file_path, file_name)
 
     # website to crawl
-    # website = "https://markets.ft.com/data/indices/ajax/getindexconstituents?xid=575769&pagenum=26"
-    # website = "https://markets.ft.com/data/indices/tearsheet/constituents?s=INX:IOM"
+    # the following is the AJAX URL used by Financial Times
     website = "https://markets.ft.com/data/indices/ajax/getindexconstituents"
     payload = {'xid': '575769', 'pagenum': 1}
 
@@ -52,8 +69,11 @@ if __name__ == '__main__':
                 print 'Equities: '
                 pprint(equities)
         except requests.exceptions.ConnectionError:
-            print "Connection reset"
-            print "Waiting"
+            print "Connection reset, waiting"
             time.sleep(5)
             print "Trying again"
 
+
+if __name__ == '__main__':
+    # main()
+    print get_xid(raw_input("Index to find> "))
