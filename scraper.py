4e54a0eb2039dbb0dcc09f8b60b6619671b66834
james0x41 <james0x41@gmail.com>
2016-11-27 21:28:10 +0800

    reformat main and add skeleton for get_xid method

--- scraper.py
+++ scraper.py
@@ -8,20 +8,29 @@ import requests
 from pprint import pprint
 
 
+# get xid from an index (i.e INX:IOM, FTSE:FSI, etc)
+def get_xid(index):
+    payload = {'s': index}
+    website = "https://markets.ft.com/data/indices/tearsheet/constituents"
+    page = requests.get(website, params=payload)
+    tree = html.fromstring(page.content)
+    xid = tree.xpath('//div[@class=')
 
-# website = "https://markets.ft.com/data/indices/tearsheet/constituents?s=INX:IOM"
-payload = {'xid': '575769', 'pagenum': 26}
-# website = "https://markets.ft.com/data/indices/ajax/getindexconstituents?xid=575769&pagenum=26"
-website = "https://markets.ft.com/data/indices/ajax/getindexconstituents"
 
-# retrieve the website with the data
-# parse it and save as a tree
-page = requests.get(website, params=payload)
-tree = html.fromstring(page.json()['html'])
-# tree = html.fromstring(page.content)
+if __name__ == '__main__':
+    # website = "https://markets.ft.com/data/indices/tearsheet/constituents?s=INX:IOM"
+    payload = {'xid': '575769', 'pagenum': 26}
+    # website = "https://markets.ft.com/data/indices/ajax/getindexconstituents?xid=575769&pagenum=26"
+    website = "https://markets.ft.com/data/indices/ajax/getindexconstituents"
 
-# get the list of equities
-equities = tree.xpath('//td/a[@class="mod-ui-link"]/text()')
+    # retrieve the website with the data
+    # parse it and save as a tree
+    page = requests.get(website, params=payload)
+    tree = html.fromstring(page.json()['html'])
+    # tree = html.fromstring(page.content)
 
-print 'Equities: '
-pprint(equities)
+    # get the list of equities
+    equities = tree.xpath('//td/a[@class="mod-ui-link"]/text()')
+
+    print 'Equities: '
+    pprint(equities)
