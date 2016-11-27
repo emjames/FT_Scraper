657cd1ee4956c08e69808f712521d9fe81883a72
james0x41 <james0x41@gmail.com>
2016-11-27 21:26:09 +0800

    retrieve equities from a given xid and page num

--- scraper.py
+++ scraper.py
@@ -8,16 +8,20 @@ import requests
 from pprint import pprint
 
 
-website = "https://markets.ft.com/data/indices/tearsheet/constituents?s=INX:IOM"
+
+# website = "https://markets.ft.com/data/indices/tearsheet/constituents?s=INX:IOM"
+payload = {'xid': '575769', 'pagenum': 26}
+# website = "https://markets.ft.com/data/indices/ajax/getindexconstituents?xid=575769&pagenum=26"
+website = "https://markets.ft.com/data/indices/ajax/getindexconstituents"
 
 # retrieve the website with the data
 # parse it and save as a tree
-page = requests.get(website)
-tree = html.fromstring(page.content)
+page = requests.get(website, params=payload)
+tree = html.fromstring(page.json()['html'])
+# tree = html.fromstring(page.content)
 
 # get the list of equities
 equities = tree.xpath('//td/a[@class="mod-ui-link"]/text()')
 
 print 'Equities: '
 pprint(equities)
-
